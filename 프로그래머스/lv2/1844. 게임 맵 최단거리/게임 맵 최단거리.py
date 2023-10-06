afa287762c60e354297a6f07f2eb271796350feb
박정균 <park32323@gmail.com>
2023-10-07 00:08:03 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.48 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,51 @@
+from copy import deepcopy
+
+def is_valid_position(i, j, maps):
+    return i >= 0 and i < len(maps) and j >= 0 and j < len(maps[0])
+
+def available_directions(i, j, maps):
+    available = []
+    for i_offset, j_offset in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+        i_n = i + i_offset
+        j_n = j + j_offset
+        if is_valid_position(i_n, j_n, maps) and maps[i_n][j_n]:
+            available.append((i_n, j_n))
+    return available
+
+def DFS(i, j, maps):
+    # Terminate condition
+    if i == len(maps)-1 and j == len(maps[0])-1:
+        return 1
+
+    # Move (Only to opened directions)
+    paths = []
+    for i_n, j_n in available_directions(i, j, maps):
+        new_maps = deepcopy(maps)
+        new_maps[i_n][j_n] = 0
+        if dist:=DFS(i_n, j_n, new_maps):
+            paths.append(dist)
+        # print((i_n, j_n), dist)
+    if paths:
+        return min(paths) + 1
+    else:
+        return None
+
+def BFS(maps):
+    queue = [(0,0,0)] # (i, j, map, depth)
+    while queue:
+        i, j, depth = queue.pop(0)
+        if i+1 == len(maps) and j+1 == len(maps[0]):
+            return depth + 1
+        for i_n, j_n in available_directions(i, j, maps):
+            maps[i_n][j_n] = 0
+            queue.append((i_n, j_n, depth+1))
+    return -1
+
+def solution(maps):
+    # maps[0][0] = 0
+    # answer = DFS(0, 0, maps)
+    # answer = answer if answer is not None else -1
+    
+    answer = BFS(maps)
+    
+    return answer
